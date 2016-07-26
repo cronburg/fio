7b5c0f50d9e42c41fac950c353917fceb4ce72ff
Karl Cronburg <kcronbur@redhat.com>
2016-07-26 13:40:54 -0400

    degrade gracefully when cython or the pyx file aren't present

--- tools/fiologparser_numpy.py
+++ tools/fiologparser_numpy.py
@@ -24,8 +24,53 @@ import argparse
 import os
 import pandas
 
-import pyximport; pyximport.install()
-from fio_generator import fio_generator
+try:
+    import pyximport; pyximport.install()
+    from fio_generator import fio_generator
+except ImportError:
+    """ User does not have Cython installed, or is otherwise missing the
+        fio_generator.pyx file. Skip cythonizing by putting code directly here too: """
+
+    import re
+    err = sys.stderr.write
+    row_re = re.compile("^\d+,\s\d+,\s\d+,\s\d+\s+$")
+
+    def next_no_stop_iter(fp):
+        try:
+            while True:
+                line = fp.next()
+                if row_re.match(line):
+                    return line
+                err("WARNING: Ignoring line '%s' - does not match expected format.\n" % line.rstrip())
+        except StopIteration:
+            return None
+
+    def get_time(line):
+        try:
+            return int(line.split(',')[0])
+        except ValueError:
+            return None
+
+    def get_min(lines, fps):
+        mn_fp, mn_time = None, None
+        for fp in fps:
+            line = lines.get(fp)
+            if not line is None:
+                time = get_time(line)
+                if (not time is None) and (mn_time is None or time < mn_time):
+                    mn_time, mn_fp = time, fp
+        return mn_fp
+
+    def fio_generator(fps):
+        """ Create a generator for reading multiple fio files in end-time order """
+        lines = {fp: next_no_stop_iter(fp) for fp in fps}
+
+        while True:
+            # Get fp with minimum value in the first column (fio log end-time value)
+            fp = get_min(lines, fps)
+            if fp is None: break
+            yield (lines[fp].rstrip() + ", " + str(fps.index(fp)) + '\n')
+            lines[fp] = next_no_stop_iter(fp) # read a new line into our dictionary
 
 debug = not (os.getenv("DEBUG") is None)
 err = sys.stderr.write
