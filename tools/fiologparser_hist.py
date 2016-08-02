fio histograms max out at 16.7 seconds, so setting default max_latency to 20 seconds for now. Also fixing weighted average calculation

tools/fiologparser_hist.py
@@ -13,7 +13,8 @@
 import sys
 import pandas
 import numpy as np
-from fiologparser_numpy import weights, columns, weighted_percentile, percs, fmt_float_list
+from fiologparser_numpy import weights, columns, weighted_percentile \
+                            , weighted_average, percs, fmt_float_list
 
 __HIST_COLUMNS = 1216
 __NON_HIST_COLUMNS = 3
@@ -119,7 +120,7 @@ def plat_idx_to_val(idx, FIO_IO_U_PLAT_BITS=6, FIO_IO_U_PLAT_VAL=64):
 def print_all_stats(ctx, end, ss_cnt, mn, vs, ws, mx):
     ps = weighted_percentile(percs, vs, ws)
 
-    avg = np.sum(vs * ws) / ss_cnt
+    avg = weighted_average(vs, ws)
     values = [mn, avg] + list(ps) + [mx]
     row = [end, ss_cnt] + map(lambda x: float(x) / ctx.divisor, values)
     fmt = "%d, %d, " + fmt_float_list(ctx, 7)
@@ -213,7 +214,7 @@ if __name__ == '__main__':
        , help='number of samples to buffer into numpy at a time'
        )
     arg( '--max_latency'
-       , default=300
+       , default=20
        , type=float
        , help='number of seconds of data to process at a time'
        )

tools/fiologparser_numpy.py
@@ -121,6 +121,9 @@ def weights(start_ts, end_ts, start, end):
     ws[np.where(np.isnan(ws))] = 0.0;
     return ws
 
+def weighted_average(vs, ws):
+    return np.sum(vs * ws) / np.sum(ws)
+
 columns = ["end-time", "samples", "min", "avg", "median", "90%", "95%", "99%", "max"]
 percs   = [50, 90, 95, 99]
 
@@ -134,7 +137,7 @@ def fmt_float_list(ctx, num=1):
 
 def print_sums(ctx, vs, ws, ss, end, divisor=1.0):
     fmt = "%d, " + fmt_float_list(ctx, 1)
-    print (fmt % (end, np.sum(vs * ws) / divisor / ctx.divisor))
+    print (fmt % (end, weighted_average(vs, ws) / divisor / ctx.divisor))
 
 def print_averages(ctx, vs, ws, ss, end):
     print_sums(ctx, vs, ws, ss, end, divisor=float(len(vs)))
@@ -155,7 +158,7 @@ def print_full(ctx, vs, ws, ss, end):
 def print_all_stats(ctx, vs, ws, ss_cnt, end):
     ps = weighted_percentile(percs, vs, ws)
 
-    values = [np.min(vs), sum(vs * ws) / sum(ws)] + list(ps) + [np.max(vs)]
+    values = [np.min(vs), weighted_average(vs, ws)] + list(ps) + [np.max(vs)]
     row = [end, ss_cnt] + map(lambda x: float(x) / ctx.divisor, values)
     fmt = "%d, %d, " + fmt_float_list(ctx, 7)
     print (fmt % tuple(row))