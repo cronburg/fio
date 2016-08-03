1e157b8bad188e0450f743e50807ef0149319dfe
Karl Cronburg <kcronbur@redhat.com>
2016-08-03 12:21:01 -0400

    generalized halving of number of bins to any level of coarseness

--- tools/hist/half-bins.py
+++ tools/hist/half-bins.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python2.7
 import sys
 
-# Cut the number bins in half in fio histogram output
+""" Cut the number bins in half in fio histogram output """
 
-with open(sys.argv[1], 'r') as fp:
-    for line in fp.readlines():
-        vals = line.split(', ')
-        sys.stdout.write("%s, %s, %s, " % tuple(vals[:3]))
+def main(ctx):
+    stride = 1 << ctx.coarseness
+    with open(ctx.FILENAME, 'r') as fp:
+        for line in fp.readlines():
+            vals = line.split(', ')
+            sys.stdout.write("%s, %s, %s, " % tuple(vals[:3]))
 
-        hist = list(map(int, vals[3:]))
-        for i in range(0, len(hist) - 2, 2):
-            sys.stdout.write("%d, " % (hist[i] + hist[i+1],))
-        sys.stdout.write("%d\n" % (hist[len(hist) - 2] + hist[len(hist) - 1]))
-        
+            hist = list(map(int, vals[3:]))
+            for i in range(0, len(hist) - stride, stride):
+                sys.stdout.write("%d, " % sum(hist[i : i + stride],))
+            sys.stdout.write("%d\n" % sum(hist[len(hist) - stride:]))
+
+if __name__ == '__main__':
+    import argparse
+    p = argparse.ArgumentParser()
+    arg = p.add_argument
+    arg( 'FILENAME', help='clat_hist file for which we will reduce'
+                         ' (by half or more) the number of bins.')
+    arg( '-c', '--coarseness'
+       , default=1
+       , type=int
+       , help='number of times to reduce number of bins by half, '
+              'e.g. coarseness of 4 merges each 2^4 = 16 consecutive '
+              'bins.'
+       )
+    main(p.parse_args())
+
